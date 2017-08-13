0e01c9c5fe118b24b28bd04be703a23c2de00ffc
preet2fun <preet2fun@yahoo.com>
2017-08-13 16:51:29 -0700

    dock string updated

--- calci.py
+++ calci.py
@@ -1,12 +1,11 @@
-
 #!/usr/bin/env python
 """This module provides functions for authenticating users."""
-
-
 
 import time
 
 class Calculator:
+    """ calculator class """
     def sum(self, a, b):
+	""" testing .."""
         time.sleep(10) # long running process
         return a + b
--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 """Unit tests for the project1.calculator module."""
 
-
-
 from unittest import TestCase
 from mock import patch
-import Test_Project.calci
+#import Test_Project.calci
 
 class TestCalculator(TestCase):
+	"""abcd... """
     @patch('Test_Project.calci.Calculator.sum', return_value=9)
+	""" def... """
     def test_sum(self, sum):
         self.assertEqual(sum(2,3), 9)
